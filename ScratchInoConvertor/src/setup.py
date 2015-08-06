6c39daa1a8c90429f2a756a6675f36d953de2394
unknown <a.lamande@LEOF-ALA1.ad.lbn.fr>
2015-08-06 12:03:46 +0200

    other: ignore dll for compilation

--- ScratchInoConvertor/src/setup.py
+++ ScratchInoConvertor/src/setup.py
@@ -1,4 +1,5 @@
 from distutils.core import setup
 import py2exe
 
-setup(windows=['ScratchInoConv.py'])
+setup(windows=['ScratchInoConv.py'],
+      options={"py2exe":{"dll_excludes":["MSVCP90.dll"]}})
