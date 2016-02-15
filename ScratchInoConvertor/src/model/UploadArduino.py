aafd55a821a894d288c995b215792dcbd44c9d2f
root <tristanbouju@gmail.com>
2016-02-15 23:19:04 +0100

    UploadArduino class is now a thread add comment

--- ScratchInoConvertor/src/model/UploadArduino.py
+++ ScratchInoConvertor/src/model/UploadArduino.py
@@ -11,8 +11,10 @@ from re import search
 
 # TODO add exception
 #   - user not in group tty… to upload to the board (_linux)
-import serial
+from time import sleep
 
+import serial
+import threading
 from model.Message import Message
 
 
@@ -20,13 +22,27 @@ class UploadArduinoError(Exception):
     pass
 
 
-class UploadArduino:
+class UploadArduino(threading.Thread):
 
-    def __init__(self):
+    def __init__(self, arduino_type, serial_port, arduino_file, callback_log=None):
+        """
+        upload an arduino code to the arduino board
+        :param arduino_type: the arduino model: uno, mega…
+        :param serial_port: the serial port where the arduino board is connected
+        :param arduinofile: the arduino code to upload
+        :callback_log: Message(code, message), see the Message class
+        """
+
+        super(UploadArduino, self).__init__()
+
         # TODO link every board name to package:arch:board
         # https://github.com/arduino/_arduino/blob/ide-1.5.x/build/shared/manpage.adoc
-        self.__arduino_type = {"uno": "arduino:avr:uno"}
-        pass
+        self.__arduino_type_list = {"uno": "arduino:avr:uno"}
+
+        self.__arduino_type = arduino_type
+        self.__serial_port = serial_port
+        self.__arduino_file = arduino_file
+        self.__callback_log = callback_log
 
     def __arduino_ui_is_installed_linux(self):
         """
@@ -73,66 +89,75 @@ class UploadArduino:
         """
         arduino_type = arduino_type.lower()
 
-        for key in self.__arduino_type:
+        for key in self.__arduino_type_list:
             arduino_arch = search(key, arduino_type)
             if arduino_arch:
-                return self.__arduino_type[key]
+                return self.__arduino_type_list[key]
 
         raise UploadArduinoError("Error: the Arduino board architecture is unknown, please contact us to support it")
 
-    def upload(self, arduino_type, serial_port, arduinofile, callback_log=None):
+    def run(self):
         """
         upload an arduino code to the arduino board
-        :param arduino_type: the arduino model: uno, mega…
-        :param serial_port: the serial port where the arduino board is connected
-        :param arduinofile: the arduino code to upload
         """
-
-        print arduino_type, serial_port, arduinofile
 
         # get arduino executable path
         if 'nt' in sys.builtin_module_names:
             import _winreg
             arduino_exe = self.__arduino_ui_get_path_windows()
+
         elif 'posix' in sys.builtin_module_names:
             self.__arduino_ui_is_installed_linux()
             arduino_exe = "arduino"
+
         else:
             raise UploadArduinoError("Error: unsupported OS")
 
         # test if the file exist
-        if not os.path.isfile(arduinofile):
+        if not os.path.isfile(self.__arduino_file):
             raise IOError("Error: no such file")
 
         # get the architecture
-        arduino_arch = self.__get_arduino_arch_board(arduino_type)
+        arduino_arch = self.__get_arduino_arch_board(self.__arduino_type)
 
         # test if serial is reachable
         # exception is thrown else
-        ser = serial.Serial(serial_port, 4600, timeout=1)
+        ser = serial.Serial(self.__serial_port, 4600, timeout=1)
         ser.close()
 
         # prepare the command
         command = [arduino_exe, '--board', arduino_arch, '--port',
-                   serial_port, '--upload', arduinofile]
+                   self.__serial_port, '--upload', self.__arduino_file]
 
         # execute and display
         proc = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        # proc_stdout = proc.stdout.read()
 
+        # concat because the ui doesn't like
+        # receiving line one after the other
+        lines = ""
         for line in iter(proc.stderr.readline, b''):
-            if callback_log is None:
-                print line
-            else:
-                callback_log(Message(Message.CONSOLE_LOG_ERR, line))
+            lines = lines + line
+        print lines
 
+        lines = ""
         for line in iter(proc.stdout.readline, b''):
-            if callback_log is None:
-                print line
+            lines = lines + line
+        print lines
+
+        # wait and get the script status
+        # print if no callback, else give to callback
+        proc.wait()
+        if proc.poll() == 0:
+            if self.__callback_log is None:
+                print "SUCCEED"
             else:
-                callback_log(Message(Message.CONSOLE_LOG, line))
+                self.__callback_log(Message(Message.SUCCEED_MESSAGE))
+        else:
+            if self.__callback_log is None:
+                print "SUCCEED"
+            else:
+                self.__callback_log(Message(Message.ERROR_MESSAGE))
 
 if __name__ == '__main__':
-    upload_arduino = UploadArduino()
-    upload_arduino.upload("uno", "/dev/ttyACM0", "/home/battosai/Arduino/buzzer/buzzer.ino")
-    # upload_arduino.upload("uno", "COM5", "D:\\Mes Documents\\Arduino\\buzzer\\buzzer.ino")
+    upload_arduino = UploadArduino("uno", "/dev/ttyACM0", "/home/battosai/Arduino/buzzer/buzzer.ino")
+    upload_arduino.start()
